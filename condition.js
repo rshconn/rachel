4cb18f8d62c4df20c0c62b9380e2cd94cd4c7427
Rachel Connolly <rshconn@umich.edu>
2017-07-12 11:24:55 -0400

    added rand function

---
+++ condition.js
@@ -0,0 +1,7 @@
+var level = 10;
+
+if (level % 2 == 0) {   // even
+    console.log("You're on an even level");
+} else {                // odd
+    console.log("You're on an odd level");
+}
---
+++ rachel.js
@@ -0,0 +1,6 @@
+function rand(num) {
+    var randDec = Math.random() * num;
+    var randInt = Math.floor(randDec);
+    return randInt;
+}
+
