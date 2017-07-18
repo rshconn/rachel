c6a7de49821b5d2e5e70ddd1d06398ff881711ca
Rachel Connolly <rshconn@umich.edu>
2017-07-18 09:58:30 -0400

    first commit

--- condition.js
+++
@@ -1,7 +0,0 @@
-var level = 10;
-
-if (level % 2 == 0) {   // even
-    console.log("You're on an even level");
-} else {                // odd
-    console.log("You're on an odd level");
-}
--- rachel.js
+++
@@ -1,6 +0,0 @@
-function rand(num) {
-    var randDec = Math.random() * num;
-    var randInt = Math.floor(randDec);
-    return randInt;
-}
-
